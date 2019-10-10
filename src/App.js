a2044e0672104848f426367e0a10a86730da36dd
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-10-10 12:18:57 -0500

    Updated handleDelete to remove the card from allCards when no lists contain it. Co-authored-by: JonnyDeates <jonnydeates@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,68 +3,74 @@ import List from './List'
 import './App.css';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            lists: [
-                {
-                    id: '1',
-                    header: 'First list',
-                    cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
-                },
-                {
-                    id: '2',
-                    header: 'Second list',
-                    cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
-                },
-                {
-                    id: '3',
-                    header: 'Third list',
-                    cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
-                },
-                {
-                    id: '4',
-                    header: 'Fourth list',
-                    cardIds: [ 'l', 'm' ],
-                },
-            ],
-            allCards: {
-                'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
-                'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
-                'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
-                'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
-                'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
-                'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
-                'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
-                'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
-                'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
-                'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
-                'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
-                'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
-                'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
-            },
-        }
+  constructor() {
+    super();
+    this.state = {
+      lists: [
+        {
+          id: '1',
+          header: 'First list',
+          cardIds: ['a', 'b', 'e', 'f', 'g', 'j', 'l', 'm'],
+        },
+        {
+          id: '2',
+          header: 'Second list',
+          cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+        },
+        {
+          id: '3',
+          header: 'Third list',
+          cardIds: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'],
+        },
+        {
+          id: '4',
+          header: 'Fourth list',
+          cardIds: ['l', 'm'],
+        },
+      ],
+      allCards: {
+        'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+        'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+        'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+        'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+        'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+        'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+        'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+        'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+        'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+        'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+        'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+        'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+        'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
+      },
     }
-    handleDelete = (id, listid) => {
-        const newList = this.state.lists.find(item => item.id === listid);
-        newList.cardIds = this.state.lists.find(item => item.id === listid).cardIds.filter(card => card !== id);
-      this.setState({lists: [...this.state.lists], allCards: {...this.state.allCards}});
+  }
+  handleDelete = (id, listid) => {
+    const newList = this.state.lists.find(item => item.id === listid);
+    newList.cardIds = this.state.lists.find(item => item.id === listid).cardIds.filter(card => card !== id);
+    let foundIt = this.state.lists.filter(list => (list.cardIds.includes(id) === true))
+
+    if (foundIt.length === 0) {
+      delete this.state.allCards[id]
+    }
+
+    this.setState({ lists: [...this.state.lists], allCards: { ...this.state.allCards } });
+  };
+
+  newRandomCard = (listid) => {
+    const id = Math.random().toString(36).substring(2, 4)
+      + Math.random().toString(36).substring(2, 4);
+    const newList = this.state.lists.find(item => item.id === listid);
+    newList.cardIds.push(id);
+
+    const newCard = {
+      id,
+      title: `Random Card ${id}`,
+      content: 'lorem ipsum'
     };
 
-    newRandomCard = (listid) => {
-        const id = Math.random().toString(36).substring(2, 4)
-            + Math.random().toString(36).substring(2, 4);
-        const newList = this.state.lists.find(item => item.id === listid);
-        newList.cardIds.push(id);
-
-        const newCard = {
-            id,
-            title: `Random Card ${id}`,
-            content: 'lorem ipsum'
-        };
-
-        this.setState({lists: [...this.state.lists], allCards: {...this.state.allCards, [newCard.id]: newCard}})
-    };
+    this.setState({ lists: [...this.state.lists], allCards: { ...this.state.allCards, [newCard.id]: newCard } })
+  };
 
   render() {
     return (
